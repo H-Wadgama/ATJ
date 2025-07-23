d2b089ecd9bfdbda482ce4bbe3dce4156697f0e8
Hafi Wadgama <mmw6427@psu.edu>
2025-07-23 16:21:13 -0400

    adding useful code snippets that can help me

---
+++ atj_saf/atj_baseline/units/dehydration-detailed.py
@@ -0,0 +1,8 @@
+import qsdsan as qs
+import biosteam as bst
+import thermosteam as tmo
+import biorefineries as bf
+tmo.Stream.display_units.N = 20 
+from qsdsan.sanunits import _heat_exchanging 
+import pandas as pd
+
