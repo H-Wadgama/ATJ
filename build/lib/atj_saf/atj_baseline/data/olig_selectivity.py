b338ba3534fe8b71461e2e3075af54e68ebbb066
Hafi Wadgama <mmw6427@psu.edu>
2025-07-25 10:39:55 -0400

    working rigorous=True

---
+++ build/lib/atj_saf/atj_baseline/data/olig_selectivity.py
@@ -0,0 +1,6 @@
+biofuel_composition = {              # More info in the OligomerizationReactor class doc.
+                    'C4H8'  : 0.2,          # 0.2
+                    'C6H12' : 0.15,          # 0.15
+                    'C10H20': 0.62,          # 0.62
+                    'C18H36': 0.03,          # 0.03
+                    }           
