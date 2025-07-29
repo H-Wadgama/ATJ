320bdf0f25875f1c195f2982578bde9d75d84964
Hafi Wadgama <mmw6427@psu.edu>
2025-07-29 14:00:35 -0400

    working baseline biorefinery

--- atj_saf/main.py
+++ atj_saf/main.py
@@ -9,8 +9,15 @@ def main():
     atj_system.show()
 
     baseline_tea = perform_tea()
-    print(f"\n: {baseline_tea.NPV}")
 
+
+    saf_stream = atj_system.flowsheet.stream['SAF_product']
+    saf_density = saf_stream.rho
+                                 
+    mjsp_usd_kg = baseline_tea.solve_price(saf_stream)
+    mjsp_usd_gal = (mjsp_usd_kg*saf_density)/264.172
+
+    print(f'The MJSP is {round((baseline_tea.solve_price(saf_stream)*saf_stream.rho)/264.172,2)} USD/gal')
 
 '''
     print("✅ Simulation complete!")
